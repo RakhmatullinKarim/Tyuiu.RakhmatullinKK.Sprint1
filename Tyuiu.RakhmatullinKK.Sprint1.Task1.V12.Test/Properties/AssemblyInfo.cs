14b74129bde81b4a9e8f426522a0a00f20bcaf8f
Tyuiu.RakhmatullinKK@outlook.com <Tyuiu.RakhmatullinKK@outlook.com>
2023-09-20 23:39:58 +0500

    Task1

---
+++ Tyuiu.RakhmatullinKK.Sprint1.Task1.V12.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.RakhmatullinKK.Sprint1.Task1.V12.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.RakhmatullinKK.Sprint1.Task1.V12.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5d3985e3-1304-4665-a917-d5ee0b6b4806")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- Tyuiu.RakhmatullinKK.Sprint1.Task1.V12/Program.cs
+++ Tyuiu.RakhmatullinKK.Sprint1.Task1.V12/Program.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tyuiu.RakhmatullinKK.Sprint1.Task1.V12.Lib;
 
-using Tyuiu.RakhmatullinKK.Sprint1.Task0.V29.Lib;
-
-namespace Tyuiu.RakhmatullinKK.Sprint1.Task0.V29
+namespace Tyuiu.RakhmatullinKK.Sprint1.Task1.V12
 {
     class Program
     {
@@ -18,22 +17,32 @@ namespace Tyuiu.RakhmatullinKK.Sprint1.Task0.V29
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #1                                                               *");
             Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
-            Console.WriteLine("* Задание #0                                                              *");
-            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #12                                                             *");
             Console.WriteLine("* Выполнил: Рахматуллин Карим Камильевич | АСОиУб-23-2                     *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Написать программу, которая вычисляет значения выражения                *");
-            Console.WriteLine("* и печатает результат на экране.                                         *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по форумле (x + y) / 6 и печатает его на экран*");
             Console.WriteLine("*                                                                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* 2 * 9 + 7 * 2                                                              *");
+
+            double x, y;
+
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine(ds.Calculate());
+
+            Console.WriteLine(ds.Calculate(x, y));
+
             Console.ReadKey();
         }
     }
