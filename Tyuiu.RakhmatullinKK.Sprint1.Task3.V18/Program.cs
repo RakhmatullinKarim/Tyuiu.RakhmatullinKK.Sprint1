9439c957fde136fc4dbd38318db685d1086a03db
Tyuiu.RakhmatullinKK@outlook.com <Tyuiu.RakhmatullinKK@outlook.com>
2023-09-22 21:42:59 +0500

    Task3

---
+++ Tyuiu.RakhmatullinKK.Sprint1.Task3.V18/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.RakhmatullinKK.Sprint1.Task3.V18.Lib;
+
+namespace Tyuiu.RakhmatullinKK.Sprint1.Task3.V18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Рахматуллин К.К. | АСОиУб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнил: Рахматуллин Карим Камильевич | АСОиУб-23-2                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя стороны A, B, C  *");
+            Console.WriteLine("* и находит количество квадратов в прямоугольнике                         *");
+            Console.WriteLine("* с размерами АхВ                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double a, b, c;
+
+            Console.WriteLine("Введите значение A:");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение B:");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение C:");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(Math.Floor(ds.HowManySquares(a, b, c)));
+
+            Console.ReadKey();
+        }
+    }
+}
